7c007387e1f52609113773f13fe760ca631a9513
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2025-07-02 13:54:35 +0200

    worked with built in modles in python

---
+++ modules_builtin.py
@@ -0,0 +1,15 @@
+import math
+import random
+import datetime
+import os 
+import sys
+
+print(sys.version)
+
+print(os.getcwd)
+
+print(datetime.datetime.now())
+
+#print(random.randint(0,100)) #randomize
+
+#print(math.pi) #math Pi
