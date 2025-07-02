814161e63d034af615ff8591eaed892b0d508f1d
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2025-07-02 10:40:58 +0200

    first commit

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+#dictionary
+
+myDictionary = {'apple': 3 ,'banana':5, 'orange':2}
+
+print(myDictionary['apple'])
+
+myDictionary['grapes'] = 4
+print(myDictionary)
+
+myDictionary['banana'] = 7
+print(myDictionary)
---
+++ tuple.py
@@ -0,0 +1,12 @@
+#tuples( they are unchangeable)
+
+
+my_tuple = (1,2,3,4,5)
+print(my_tuple)
+print(my_tuple[-1])
+
+
+tuple1 =(1,2)
+tuple2 =(3,4)
+conc_tuple = tuple1 + tuple2
+print(conc_tuple)
